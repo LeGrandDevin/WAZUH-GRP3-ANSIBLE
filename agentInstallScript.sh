301efef86bb05d1c46c17c74c7474f5fa2018297
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2023-07-09 06:44:02 +0200

    Update agentInstallScript.sh

--- agentInstallScript.sh
+++ agentInstallScript.sh
@@ -1,10 +1,12 @@
+#Ask for credentials
+read -p 'Remote-system/Manager IP Address: ' manager_ipaddr
 read -p 'Agent IP Address: ' agent_ipaddr
 
 #Send ssh key to the wazuh server
 cat ~/.ssh/id_rsa.pub | ssh root@${agent_ipaddr} "cat >> ~/.ssh/authorized_keys"
 
 #Create and configure agent playbook
-python3 playbookAgent.py $agent_ipaddr
+python3 playbookAgent.py $manager_ipaddr
 
 #Run the playbook
 ansible-playbook /etc/ansible/roles/wazuh-ansible/playbooks/wazuh-agent.yml -b -K
