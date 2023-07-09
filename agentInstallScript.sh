3408b4d834d9a3fc52489c095620f82ba4b37e5f
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2023-07-09 07:07:29 +0200

    Update agentInstallScript.sh

--- agentInstallScript.sh
+++ agentInstallScript.sh
@@ -5,6 +5,9 @@ read -p 'Agent IP Address: ' agent_ipaddr
 #Send ssh key to the wazuh server
 cat ~/.ssh/id_rsa.pub | ssh root@${agent_ipaddr} "cat >> ~/.ssh/authorized_keys"
 
+#Add role to hosts file
+python3 changeHosts.py $manager_ipaddr "[wazuh-agents]"
+
 #Create and configure agent playbook
 python3 playbookAgent.py $manager_ipaddr
 
