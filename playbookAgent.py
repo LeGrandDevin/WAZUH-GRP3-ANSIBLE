282b1c8181e219b0de99df71b40381bfe3ee3d4e
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2023-07-10 00:47:21 +0200

    Update playbookAgent.py

--- playbookAgent.py
+++ playbookAgent.py
@@ -17,7 +17,20 @@ agent_playbook = '''- hosts: wazuh_agents
         max_retries: 5
         retry_interval: 5
     wazuh_agent_enrollment:
-        agent_name: '{agentName}\''''.format(ipaddr = ipaddr, agentName = agentName)
+      enabled: ''
+      manager_address: ''
+      port: 1515
+      agent_name: '{agentName}'
+      groups: ''
+      agent_address: ''
+      ssl_cipher: HIGH:!ADH:!EXP:!MD5:!RC4:!3DES:!CAMELLIA:@STRENGTH
+      server_ca_path: ''
+      agent_certificate_path: ''
+      agent_key_path: ''
+      authorization_pass_path: /var/ossec/etc/authd.pass
+      auto_method: 'no'
+      delay_after_enrollment: 20
+  use_source_ip: 'no'\''''.format(ipaddr = ipaddr, agentName = agentName)
 f = open('/etc/ansible/roles/wazuh-ansible/playbooks/wazuh-agent.yml', 'w')
 f.write(agent_playbook)
 f.close()
